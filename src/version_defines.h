0f47b4d6f0f27104aee042115917e0200d7c1be8
Ilya Zhuravlev <whatever@xyz.is>
2014-12-29 21:52:17 +0400

    Bump version number and version string, just to test things.

--- src/version_defines.h
+++ src/version_defines.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define VER_CODE                    2
-#define VER_STR                     "0.0"
-#define VER_FILEVERSION             0,0,0,0
-#define VER_FILEVERSION_STR         "0.0.0.0"
+#define VER_CODE                    3
+#define VER_STR                     "0.1"
+#define VER_FILEVERSION             0,1,0,0
+#define VER_FILEVERSION_STR         "0.1.0.0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
