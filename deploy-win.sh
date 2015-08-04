82c8cbff4f0a194ca7b43a3eca866bbf735ecca3
Rory <novynn@gmail.com>
2015-08-04 15:20:47 +1200

    Fixed .exe reference

--- deploy-win.sh
+++ deploy-win.sh
@@ -3,8 +3,8 @@
 rm -rf deploy
 mkdir deploy
 cd deploy
-windeployqt.exe ../release/acquisition.exe --release --no-compiler-runtime --dir .
-cp ../release/acquisition.exe .
+windeployqt.exe ../release/acquisitionplus.exe --release --no-compiler-runtime --dir .
+cp ../release/acquisitionplus.exe .
 cp ../redist/* .
 rm -rf bearer
 echo "[Paths]\nLibraries=./plugins" > qt.conf
