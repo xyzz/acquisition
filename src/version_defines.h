0de2d71fc8e71c84fcb26d1387b8b197e2c3c61d
Rory <novynn@gmail.com>
2015-10-29 10:07:17 +1300

    Bump version number for release.

--- src/version_defines.h
+++ src/version_defines.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #define VER_CODE                    18
-#define VER_STR                     "0.3c"
-#define VER_FILEVERSION             0,3,3,0
-#define VER_FILEVERSION_STR         "0.3.3.0"
+#define VER_STR                     "0.3d"
+#define VER_FILEVERSION             0,3,4,0
+#define VER_FILEVERSION_STR         "0.3.4.0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
