b7e89be38032827dd84f761be9f7e795662a4c15
Ilya Zhuravlev <whatever@xyz.is>
2015-10-31 18:51:33 +0300

    Only migrate buyouts once

--- src/itemsmanager.cpp
+++ src/itemsmanager.cpp
@@ -136,7 +136,12 @@ void ItemsManager::OnAutoRefreshTimer() {
 }
 
 void ItemsManager::MigrateBuyouts() {
+    int db_version = data_.GetInt("db_version");
+    // Don't migrate twice
+    if (db_version != 0)
+        return;
     for (auto &item : items_)
         bo_manager_.MigrateItem(*item);
     bo_manager_.Save();
+    data_.SetInt("db_version", 1);
 }
