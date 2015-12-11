971f66e26c24adc09464c4b08287b5584dead30e
Ilya Zhuravlev <whatever@xyz.is>
2015-12-11 22:40:58 +0300

    Version bump to 0.4a

--- src/version_defines.h
+++ src/version_defines.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define VER_CODE                    18
-#define VER_STR                     "0.4"
-#define VER_FILEVERSION             0,4,0,0
-#define VER_FILEVERSION_STR         "0.4.0.0"
+#define VER_CODE                    19
+#define VER_STR                     "0.4a"
+#define VER_FILEVERSION             0,4,1,0
+#define VER_FILEVERSION_STR         "0.4.1.0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
