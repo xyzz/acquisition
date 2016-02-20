53fadd1adff7842030e97d89200defc24664905f
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-20 13:16:22 +0100

    No need for sensitive_data here

--- src/autoonline.cpp
+++ src/autoonline.cpp
@@ -102,7 +102,7 @@ AutoOnline::AutoOnline(DataStore &data, DataStore &sensitive_data) :
     sensitive_data_(sensitive_data),
     enabled_(data_.GetBool("online_enabled")),
     url_(sensitive_data_.Get("online_url")),
-    process_script_(sensitive_data_.Get("process_script")),
+    process_script_(data.Get("process_script")),
     previous_status_(true)  // set to true to force first refresh
 {
     timer_.setInterval(kOnlineCheckInterval);
@@ -162,7 +162,7 @@ void AutoOnline::SetUrl(const std::string &url) {
 
 void AutoOnline::SetRemoteScript(const std::string& path) {
     process_script_ = path;
-    sensitive_data_.Set("process_script", path);
+    data.Set("process_script", path);
 }
 
 void AutoOnline::SetEnabled(bool enabled) {
