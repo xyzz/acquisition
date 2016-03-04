60de9b6ba1eabaae841a68ce2ab151409b5c41bb
Ilya Zhuravlev <whatever@xyz.is>
2016-03-04 21:56:49 +0300

    Add Perandus leagues and bump the version to 0.5b

--- src/version_defines.h
+++ src/version_defines.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define VER_CODE                    24
-#define VER_STR                     "0.5a"
-#define VER_FILEVERSION             0,5,1,0
-#define VER_FILEVERSION_STR         "0.5.1.0"
+#define VER_CODE                    25
+#define VER_STR                     "0.5b"
+#define VER_FILEVERSION             0,5,2,0
+#define VER_FILEVERSION_STR         "0.5.2.0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
