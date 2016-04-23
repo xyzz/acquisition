a36864fb9cd7370edf8c6914a557237a4c32d257
Eric White <erwhite99-git@yahoo.com>
2016-04-23 16:30:03 -0500

    Version bump to 0.5f-beta1

--- src/version_defines.h
+++ src/version_defines.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define VER_CODE                    28
-#define VER_STR                     "0.5e"
-#define VER_FILEVERSION             0,5,5,0
-#define VER_FILEVERSION_STR         "0.5.5.0"
+#define VER_CODE                    29
+#define VER_STR                     "0.5f-beta1"
+#define VER_FILEVERSION             0,5,6,0
+#define VER_FILEVERSION_STR         "0.5.6.0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
