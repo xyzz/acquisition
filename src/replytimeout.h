5e8de3a5e5701c4821063010ac50a31c9013d3de
Ilya Zhuravlev <whatever@xyz.is>
2016-05-07 17:50:42 +0300

    Retrieve list of leagues from the PoE leagues API (attempt 2).

--- src/replytimeout.h
+++ src/replytimeout.h
@@ -14,3 +14,4 @@ private slots:
 
 const int kEditThreadTimeout = 300000; // 5 minutes, pathofexile.com can be very slow
 const int kImgurUploadTimeout = 10000; // 10s
+const int kPoeApiTimeout = 3000;
