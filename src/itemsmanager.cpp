15ef5460603da3ffc66d576080604f8675c3f41c
Ilya Zhuravlev <whatever@xyz.is>
2018-10-11 10:09:05 -0400

    fix buyout migration for 3.4.3e

--- src/itemsmanager.cpp
+++ src/itemsmanager.cpp
@@ -193,10 +193,10 @@ void ItemsManager::OnAutoRefreshTimer() {
 void ItemsManager::MigrateBuyouts() {
     int db_version = data_.GetInt("db_version");
     // Don't migrate twice
-    if (db_version == 3)
+    if (db_version == 4)
         return;
     for (auto &item : items_)
         bo_manager_.MigrateItem(*item);
     bo_manager_.Save();
-    data_.SetInt("db_version", 3);
+    data_.SetInt("db_version", 4);
 }
