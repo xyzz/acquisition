b2e9e456523cb450bf042369eaca50b3f5e19ecf
Ilya Zhuravlev <whatever@xyz.is>
2018-12-01 15:59:29 -0500

    Version bump to 0.8a

--- src/version_defines.h
+++ src/version_defines.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define VER_CODE                    39
-#define VER_STR                     "0.8"
-#define VER_FILEVERSION             0,8,0,0
-#define VER_FILEVERSION_STR         "0.8.0.0"
+#define VER_CODE                    40
+#define VER_STR                     "0.8a"
+#define VER_FILEVERSION             0,8,1,0
+#define VER_FILEVERSION_STR         "0.8.1.0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
